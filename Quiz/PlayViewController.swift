4eea438fc2e31f4fac01597ef2bf2c50909331dc
Haruka <daisy_smau@icloud.com>
2018-02-18 15:27:53 +0900

    見た目とりあえずDONE

---
+++ Quiz/PlayViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlayViewController.swift
+//  Quiz
+//
+//  Created by 伊藤 遥 on 2018/02/17.
+//  Copyright © 2018年 Haruka Ito. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //全部のボタン共通
+    @IBAction func click() {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
