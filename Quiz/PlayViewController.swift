7ff3bf16f19b999ee1bf1091002ac640d023c400
Haruka <daisy_smau@icloud.com>
2018-02-26 13:11:50 +0900

    選択肢が被らずランダムで表示されるようになった！正解判定は違う

--- Quiz/PlayViewController.swift
+++ Quiz/PlayViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class PlayViewController: UIViewController {
     var seikaiCount: Int = 0
-    var mondaiCount: Int = 0
     var mondaiArray = [[Any]]()
     let sentakushi: Int = 3
     @IBOutlet var mondaiTextView: UITextView!
@@ -24,7 +23,7 @@ class PlayViewController: UIViewController {
         // Do any additional setup after loading the view.
         var tmpArray = [[Any]]()
         
-        tmpArray.append(["しのきんさんの学部は？","工学部","医学部","法学部",3])
+        tmpArray.append(["しのきんさんの学部は？","法学部","医学部","工学部",1])
         tmpArray.append(["しのきんさんの大学は？","東京大学","慶應大学","一橋大学",1])
         
         while tmpArray.count > 0 {
@@ -37,30 +36,50 @@ class PlayViewController: UIViewController {
     }
     
     func nextQuestion() {
-      var tmpArray = mondaiArray[0] as! [Any]
-        mondaiTextView.text = tmpArray[0] as! String
+      var currentMondaiArray = mondaiArray[0] as! [Any]
+        mondaiTextView.text = currentMondaiArray[0] as! String
 //        答えの要素番号を別の変数に代入しておく
 //        配列を用意してtmpArray[1]~[3]までをバラバラの順番で入れる(arc4randomで要素番号持ってきてリムーブする)
 //        その時の乱数が答えの要素番号と等しければmondaiArray[0][4]をその要素番号に変更する
+        let answer = currentMondaiArray[1]
+        let wrong1 = currentMondaiArray[2]
+        let wrong2 = currentMondaiArray[3]
         
-        let seikai: Int = tmpArray[4] as! Int
-        var tmp2Array = [Any]()
+        var tmpArray = [Any]()
         
-        var i: Int = sentakushi
-        while i > 0 {
-            let index = Int(arc4random()) % i
-            tmp2Array.append(tmpArray[index+1])
-            if index+1 == seikai {
-                mondaiArray[0][4] = tmp2Array.count
-            }
+        tmpArray.append(answer)
+        tmpArray.append(wrong1)
+        tmpArray.append(wrong2)
+        
+        var sentakushiArray = [Any]()
+        
+        while tmpArray.count > 0 {
+            let index = Int(arc4random()) % tmpArray.count
+            sentakushiArray.append(tmpArray[index])
             tmpArray.remove(at: index)
-            i -= 1
-            
         }
-            
-        sentakushi1.setTitle(tmp2Array[0] as? String, for: .normal)
-        sentakushi2.setTitle(tmp2Array[1] as? String, for: .normal)
-        sentakushi3.setTitle(tmp2Array[2] as? String, for: .normal)
+        
+        
+        
+        
+//        let seikai: Int = tmpArray[4] as! Int
+//        var tmp2Array = [Any]()
+//
+//        var i: Int = sentakushi
+//        while i > 0 {
+//            let index = Int(arc4random()) % i
+//            tmp2Array.append(tmpArray[index+1])
+//            if index+1 == seikai {
+//                mondaiArray[0][4] = tmp2Array.count
+//            }
+//            tmpArray.remove(at: index)
+//            i -= 1
+//
+//        }
+//
+        sentakushi1.setTitle(sentakushiArray[0] as? String, for: .normal)
+        sentakushi2.setTitle(sentakushiArray[1] as? String, for: .normal)
+        sentakushi3.setTitle(sentakushiArray[2] as? String, for: .normal)
         
     }
     
